d04cfc12440e22f64d7220b907fe4d2bd0de3a18
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2020-11-09 21:56:03 +0100

    added grocery-adding request basics

--- src/groceries/pages/groceries.py
+++ src/groceries/pages/groceries.py
@@ -1,10 +1,14 @@
 """Page listing the possible groceries"""
-from os import path
-from flask import render_template
+from flask import render_template, url_for, redirect, request
 from groceries.app import engine, app
-from groceries.util import local_path
 
 
 @app.route("/groceries")
 def grocerypage():
     return render_template("groceries.html", groceries=engine.grocery_list.to_list())
+
+
+@app.route("/add_groceries", methods=['POST'])
+def add_groceries():
+    engine.grocery_list.add_items_to_grocery_list(request.form.keys())
+    return redirect(url_for("grocerypage"))
