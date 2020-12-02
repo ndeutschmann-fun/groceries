892d1f57cabd51ce2609c9c4150dde6d1797b06d
Mathilde Dumond <mathilde.dumond@gmail.com>
2020-12-02 22:51:24 +0000

    added css and a base template

--- src/groceries/app/base_app.py
+++ src/groceries/app/base_app.py
@@ -4,5 +4,6 @@ from flask import Flask
 import groceries
 
 template_dir = os.path.join(os.path.dirname(groceries.__file__), "pages", "templates")
-app = Flask(__name__, template_folder=template_dir)
+static_dir = os.path.join(template_dir, "static")
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.config.update(EXPLAIN_TEMPLATE_LOADING=True)
